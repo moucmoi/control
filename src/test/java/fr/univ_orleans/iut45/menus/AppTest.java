a837885097f1d462914a77d0e7b9a0135e5ac7ed
Joaquin "Joe" Lapin <Jo.Lapin@univ-hochmoehrefelder.fr>
2024-03-24 11:29:35 +0100

    Version initiale avec maven

---
+++ src/test/java/fr/univ_orleans/iut45/menus/AppTest.java
@@ -0,0 +1,20 @@
+package fr.univ_orleans.iut45.menus;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
