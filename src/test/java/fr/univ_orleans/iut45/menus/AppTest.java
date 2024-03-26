ddc1c4cd7bd16dcbc8fecb3366e2ead06b48654f
Joaquin "Joe" Lapin <Jo.Lapin@univ-hochmoehrefelder.fr>
2024-03-26 19:01:48 +0100

    Ajout d'un test pour la classe Menu

--- src/test/java/fr/univ_orleans/iut45/menus/AppTest.java
+++ src/test/java/fr/univ_orleans/iut45/menus/AppTest.java
@@ -1,6 +1,8 @@
 package fr.univ_orleans.iut45.menus;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -17,6 +19,16 @@ public class AppTest
         allergenes_pb.add("fruits à coque");
         Plat paris_brest = new Plat ("Paris-Brest", false, allergenes_pb, 2.5);
         assertTrue(carottes_rapees.prixDeRevient() < paris_brest.prixDeRevient());
+    }
+
+    @Test
+    public void testListeEntrees()
+    {
+        Plat carottes_rapees = new Plat("Carottes Rapées", false, new ArrayList<String>(), 0.5);
+        ArrayList<Plat> les_entrees = new ArrayList<Plat>();
+        les_entrees.add(carottes_rapees);
+        Menu ce_midi = new Menu(les_entrees, new ArrayList<Plat>(), new ArrayList<Plat>());
+        assertEquals(1, ce_midi.getEntrees().size());
     }
 
 }
