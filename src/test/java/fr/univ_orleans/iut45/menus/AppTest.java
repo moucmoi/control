51e42c86d87a468c3e4db6df65421d4f14b86f20
Hajira Lajir-Haff <Hajira.Lajir-Haff@univ-lahaut.be>
2024-03-26 18:47:24 +0100

    Premier test pour la clases Plat

--- src/test/java/fr/univ_orleans/iut45/menus/AppTest.java
+++ src/test/java/fr/univ_orleans/iut45/menus/AppTest.java
@@ -1,20 +1,22 @@
 package fr.univ_orleans.iut45.menus;
 
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+import java.util.ArrayList;
+
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
+    public void testPrixPlats()
     {
-        assertTrue( true );
+        Plat carottes_rapees = new Plat("Carottes Rapées", false, new ArrayList<String>(), 0.5);
+        ArrayList<String> allergenes_pb = new ArrayList<String>();
+        allergenes_pb.add("gluten");
+        allergenes_pb.add("fruits à coque");
+        Plat paris_brest = new Plat ("Paris-Brest", false, allergenes_pb, 2.5);
+        assertTrue(carottes_rapees.prixDeRevient() < paris_brest.prixDeRevient());
     }
+
 }
